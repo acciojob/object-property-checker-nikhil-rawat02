1e1b6eba010e8c84b9d2813b747d68f9dbe27642
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-09 23:54:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for (let name of sampleObject) {
+		if (name === key) {
+			return true;
+		}
+	}
+	return false;
 }
 
 // Do not change the code below
